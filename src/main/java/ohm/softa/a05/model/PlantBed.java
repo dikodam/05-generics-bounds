944c8c430d1a4586c8e7b4eb1d2338f0fabd935c
dikodam <dikodam@googlemail.com>
2019-04-30 08:56:41 +0200

    PlantBed generified with bounds

--- src/main/java/ohm/softa/a05/model/PlantBed.java
+++ src/main/java/ohm/softa/a05/model/PlantBed.java
@@ -3,19 +3,18 @@ package ohm.softa.a05.model;
 import ohm.softa.a05.collections.SimpleList;
 import ohm.softa.a05.collections.SimpleListImpl;
 
-public class PlantBed {
-    private SimpleList<Plant> plants = new SimpleListImpl<>();
+public class PlantBed<P extends Plant> {
+    private SimpleList<P> plants = new SimpleListImpl<>();
     
-    public void add(Plant plant) {
+    public void add(P plant) {
         plants.add(plant);
-        
     }
     
     public int size() {
         return plants.size();
     }
     
-    public SimpleList<Plant> getPlantsByColor(PlantColor color) {
+    public SimpleList<P> getPlantsByColor(PlantColor color) {
         return plants.filter(item -> item.getColor() == color);
     }
 }
